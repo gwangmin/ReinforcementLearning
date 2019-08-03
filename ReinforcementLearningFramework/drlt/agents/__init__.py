9ce406cdfd87260eb3ceb04a284b934a54953eb0
gwangmin <ygm.gwangmin@gmail.com>
2019-08-03 14:45:01 +0900

    dqn, sarsa test complete

--- ReinforcementLearningFramework/drlt/agents/__init__.py
+++ ReinforcementLearningFramework/drlt/agents/__init__.py
@@ -1,2 +1,6 @@
+'''
+This package defines agents.
+'''
+
 from .DeepSARSA import DeepSARSA_Agent
 from .DQN import DQN_Agent
--- ReinforcementLearningFramework/drlt/trace/__init__.py
+++ ReinforcementLearningFramework/drlt/trace/__init__.py
@@ -1,1 +1,5 @@
+'''
+This package defines objects for trace.
+'''
+
 from .Env_wrapper import Env_wrapper
